df6c43e602e4f13ddb8c93e9e400cf59c0b55b69
arthurxz10 <arthur.santosb1090@gmail.com>
2026-03-20 22:50:55 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-desafios.py
@@ -0,0 +1,13 @@
+nome = input("Digite seu nome: ")
+print(f"Bem vindo!! {nome}")
+
+dia = input("Dia de nascimento: ")
+mes = input("mes de nascimento: ")
+ano = input("ano de nascimento: ")
+
+print(f"{dia}/{mes}/{ano}")
+
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite um numero: "))
+print(num1 + num2)
